32e2bffc9609d43f7651f64a055c707e20809f83
atienoobwanda <atienoobwanda@gmail.com>
2023-05-13 21:11:01 +0300

    reset password layout added

--- src/Pages/Users/Auth/ResetPassword.jsx
+++ src/Pages/Users/Auth/ResetPassword.jsx
@@ -1,9 +1,97 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+import { Button, Input, Line, Text } from "Components";
+import SignupColumnlogotwo from "Components/SignupColumn";
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>ResetPassword</div>
-  )
-}
+    <>
+      <div className="bg-black_900 flex flex-col font-roboto sm:gap-10 md:gap-10 gap-24 items-center justify-start mx-auto pb-[341px] w-full">
+        <SignupColumnlogotwo className="flex flex-col items-center justify-start p-8 sm:px-5 w-full" />
+        <div className="flex flex-col gap-12 items-center justify-start md:px-5 self-stretch w-auto sm:w-full">
+          <a
+            href="javascript:"
+            className="font-bold text-2xl md:text-[22px] text-left text-white_A700 sm:text-xl w-auto"
+          >
+            <Text className="">Reset password</Text>
+          </a>
+          <div className="flex flex-col gap-4 items-start justify-start self-stretch w-auto sm:w-full">
+            <div className="flex flex-col gap-4 items-center justify-start self-stretch w-auto sm:w-full">
+              <Input
+                wrapClassName="w-[73%]"
+                className="font-normal not-italic p-0 placeholder:text-white_A700 text-base text-left text-white_A700 w-full"
+                type="email"
+                name="description"
+                placeholder="Enter your email address to reset password."
+              ></Input>
+              <Line className="bg-gray_900_63 h-px w-full" />
+            </div>
+            <div className="flex flex-col gap-8 items-center justify-center self-stretch w-auto sm:w-full">
+              <div className="flex flex-col gap-6 items-center justify-center self-stretch w-auto sm:w-full">
+                <div className="flex flex-col gap-4 items-start justify-start self-stretch w-auto sm:w-full">
+                  <div className="flex flex-col gap-2 items-start justify-start self-stretch w-auto sm:w-full">
+                    <Text
+                      className="font-normal not-italic text-left text-white_A700 w-auto"
+                      variant="body4"
+                    >
+                      New password
+                    </Text>
+                    <Input
+                      wrapClassName="flex h-12 w-full"
+                      className="p-0 w-full"
+                      name="rectangleNine"
+                      placeholder=""
+                      shape="RoundedBorder4"
+                      variant="FillGray800"
+                    ></Input>
+                  </div>
+                  <div className="flex flex-col gap-2 items-start justify-start self-stretch w-auto sm:w-full">
+                    <Text
+                      className="font-normal not-italic text-left text-white_A700 w-auto"
+                      variant="body4"
+                    >
+                      Confirm password
+                    </Text>
+                    <Input
+                      wrapClassName="flex h-12 w-full"
+                      className="p-0 w-full"
+                      name="rectangleNine_One"
+                      placeholder=""
+                      shape="RoundedBorder4"
+                      variant="FillGray800"
+                    ></Input>
+                  </div>
+                </div>
+                <Button
+                  className="cursor-pointer font-bold text-center text-white_A700 text-xl w-[430px]"
+                  shape="RoundedBorder8"
+                  size="lg"
+                  variant="FillRed900"
+                >
+                  Reset password
+                </Button>
+              </div>
+              <div
+                className="common-pointer flex flex-col items-center justify-start self-stretch w-auto"
+                onClick={() => navigate("/login")}
+              >
+                <Text
+                  className="font-normal not-italic text-left text-white_A700 underline w-auto"
+                  variant="body4"
+                >
+                  Remembered my password
+                </Text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 
 export default ResetPassword
