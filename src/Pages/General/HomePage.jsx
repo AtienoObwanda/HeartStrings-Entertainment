f736eab53e3d74a57ee464b855fef5bee92996be
atienoobwanda <atienoobwanda@gmail.com>
2023-05-13 06:06:44 +0300

    Footer added

--- src/Pages/General/HomePage.jsx
+++ src/Pages/General/HomePage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 import { Button, Img, List, Text } from "Components";
 import HowToEncouragePeopleToSignUpColumnprice from "Components/HowToEncouragePeopleToSignUpColumnprice";
-import HowToEncouragePeopleToSignUpFooter from "Components/HowToEncouragePeopleToSignUpFooter";
+import Footer from "Components/HowToEncouragePeopleToSignUpFooter/Footer";
 import HowToEncouragePeopleToSignUpStreamcards from "Components/HowToEncouragePeopleToSignUpStreamcards";
 import HowToEncouragePeopleToSignUpWhitenoicon from "Components/HowToEncouragePeopleToSignUpWhitenoicon";
 import LandingPageNavbarhifi from "Components/LandingPageNavbarhifi";
@@ -16,11 +16,18 @@ const HomePage = () => {
   return (
     <>
     <div className="bg-black_900 flex flex-col font-roboto items-center justify-start mx-auto w-full">
+        {/* Navbar Section */}
         <Navbar
           className="flex flex-row items-center justify-center md:px-5 w-full"
           buyTicketS="Buy ticket (s)"
           streamPlays="Stream plays"
           account="Account"
+        />
+        {/* Hero Section */}
+
+        {/* Footer Section*/}
+        <Footer className="border-gray_800 border-solid border-t-[13px] flex items-center justify-center mt-24 md:px-5 w-full"
+          copyrightheartsOne="Copyright Heartstrings Entertainment"
         />
 
       </div>
