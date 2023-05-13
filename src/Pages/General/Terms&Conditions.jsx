a5001fa3b7320ccf42e3a228d6dfcd6698bc9634
atienoobwanda <atienoobwanda@gmail.com>
2023-05-13 08:22:30 +0300

    TC page added sucessfully

--- src/Pages/General/Terms&Conditions.jsx
+++ src/Pages/General/Terms&Conditions.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from "react-router-dom";
 import Footer from "Components/HowToEncouragePeopleToSignUpFooter/Footer";
 import Navbar from "Components/LandingPageNavbarhifi/Navbar";
+import { Text } from "Components";
 
 
 const TermsAndConditions = () => {
@@ -9,7 +10,8 @@ const TermsAndConditions = () => {
 
   return (
     <>
-    <div className="bg-black_900 flex flex-col font-roboto items-center justify-start mx-auto w-full">
+    <div className="bg-black_900 flex flex-col font-roboto items-start justify-start mx-auto w-full">
+    {/* <div className="bg-black_900 flex flex-col font-roboto items-center justify-start mx-auto w-full"> */}
         {/* Navbar Section */}
         <Navbar
           className="flex flex-row items-center justify-center md:px-5 w-full"
@@ -17,11 +19,83 @@ const TermsAndConditions = () => {
           streamPlays="Stream plays"
           account="Account"
         />
-        {/* Hero Section */}
+        {/* Main Page Content Section */}
+
+        <div className="flex flex-col gap-5 h-[1114px] md:h-auto items-start justify-start md:ml-[0] ml-[150px] mr-20 mt-24 pb-[10px] md:px-5 self-stretch w-auto md:w-full">
+          <Text
+            className="text-left text-white_A700 w-auto"
+            as="h2"
+            variant="h2"
+          >
+            Terms & Conditions
+          </Text>
+          <Text
+            className="font-normal leading-[175.00%] not-italic text-left text-white_A700"
+            as="h6"
+            variant="h6"
+          >
+            <>
+           
+
+              Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt
+              qui esse pariatur duis deserunt mollit dolore cillum minim tempor
+              enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut
+              voluptate aute id deserunt nisi.
+              <br  />
+              {/* <br  /> */}
+
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.
+              <br />
+
+              Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt
+              qui esse pariatur duis deserunt mollit dolore cillum minim tempor
+              enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut
+              voluptate aute id deserunt nisi.
+              <br />
+
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.
+              <br />
+              Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt
+              qui esse pariatur duis deserunt mollit dolore cillum minim tempor
+              enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut
+              voluptate aute id deserunt nisi.
+              <br />
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.
+              <br />
+              Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt
+              qui esse pariatur duis deserunt mollit dolore cillum minim tempor
+              enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut
+              voluptate aute id deserunt nisi.
+              <br />
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.
+              <br />
+              Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt
+              qui esse pariatur duis deserunt mollit dolore cillum minim tempor
+              enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut
+              voluptate aute id deserunt nisi.
+              <br />
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.
+              <br />
+              Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt
+              qui esse pariatur duis deserunt mollit dolore cillum minim tempor
+              enim. Elit aute irure tempor cupidatat in
+            </>
+          </Text>
+        </div>
         
 
         {/* Footer Section*/}
-        <Footer className="border-gray_800 border-solid border-t-[13px] flex items-center justify-center mt-24 md:px-5 w-full"
+        <Footer className="border-gray_800 border-solid border-t-[13px] flex items-center justify-center mt-5 md:px-5 w-full"
           copyrightheartsOne="Copyright Heartstrings Entertainment"
         />
 
