9aeebfe971f7d5fca1f0e7d04fb7a7af2a58728b
atienoobwanda <atienoobwanda@gmail.com>
2023-05-13 18:49:20 +0300

    Not Found page responsive issue fixed

--- src/Pages/General/NotFound.jsx
+++ src/Pages/General/NotFound.jsx
@@ -4,7 +4,7 @@ import Footer from "Components/Footer/Footer";
 import Navbar from "Components/Navbar/Navbar";
 
 import { Button, Img, Text } from "Components";
-import SignupColumndummylogotwo from "Components/SignupColumn";
+import SignupColumnlogotwo from "Components/SignupColumn";
 
 
 const NotFound = () => {
@@ -12,21 +12,15 @@ const NotFound = () => {
 
   return (
     <>
-    {/* <div className="bg-black_900 flex flex-col font-roboto items-center justify-start mx-auto w-full"> */}
-       
     <div className="bg-black_900 flex flex-col font-roboto sm:gap-10 md:gap-10 gap-[77px] items-center justify-start mx-auto pb-[77px] w-full">
-     {/* Navbar Section */}
-        {/* <Navbar
-          className="flex flex-row items-center justify-center md:px-5 w-full"
-          buyTicketS="Buy ticket (s)"
-          streamPlays="Stream plays"
-          account="Account"
-        /> */}
-        {/* Main Content Section */}
-        <SignupColumndummylogotwo className="flex flex-col items-center justify-start p-8 sm:px-5 w-full" />
+
+     {/* Main Page Section */}
+     <div className="bg-black_900 flex flex-col font-roboto sm:gap-10 md:gap-10 gap-[77px] items-center justify-start mx-auto pb-[30px] w-full  md:h-[1200px] sm:h-[900px]">
+        <SignupColumnlogotwo className="flex flex-col items-center justify-start p-8 sm:px-5 w-full" />
         
-         
-        <div className="flex flex-col gap-4 items-center justify-center md:px-5 self-stretch w-auto md:w-full">
+        
+        {/* Display on Large Screens  */}
+        <div className="md:hidden flex flex-col gap-4 items-center justify-center md:px-5 self-stretch w-auto md:w-full">
         <Text
             className="text-left text-white_A700 w-auto"
             as="h1"
@@ -57,9 +51,16 @@ const NotFound = () => {
             </Text>
             <Img
               src="images/img_unsplashpd4lo70ldbi.png"
+              className="h-[346px]"
+              alt="iconsaxboldlock"
+            />
+
+             {/* <Img
+              src="images/img_unsplashpd4lo70ldbi.png"
               className="h-[346px] sm:h-auto object-cover w-[299px] md:w-full"
               alt="unsplashpd4loSeventy"
-            />
+            /> */}
+
             <Text
               className="text-left text-white_A700 w-auto"
               as="h4"
@@ -79,10 +80,63 @@ const NotFound = () => {
             Go back Home
           </Button>
         </div> 
-        {/* Footer Section*/}
-        {/* <Footer className="border-gray_800 border-solid border-t-[13px] flex items-center justify-center mt-24 md:px-5 w-full"
-          copyrightheartsOne="Copyright Heartstrings Entertainment"
-        /> */}
+
+
+        {/* Display Medium Screens  */}
+        <div className="md:mt-[300px] sm:mt-[200px] hidden md:flex flex-col gap-4 items-center justify-center md:px-5 self-stretch w-auto md:w-full">
+        <Text
+            className="text-left text-white_A700 w-auto"
+            as="h1"
+            variant="h1"
+          >
+            Page not found
+          </Text>
+          <Text
+            className="font-normal leading-[175.00%] not-italic text-center text-white_A700"
+            as="h6"
+            variant="h6"
+          >
+            <>
+              Whoopsie! It looks like the page you were looking for has gone
+              missing. <br />
+              We&#39;ll keep an eye out for it.
+            </>
+          </Text>
+
+        <div className="hidden md:flex gap-6  justify-center mt-10 mb-10">
+          <Text
+              className="text-left text-white_A700 w-auto"
+              as="h1"
+              variant="h1"
+            >
+              4
+            </Text>
+            <Img
+              src="images/img_unsplashpd4lo70ldbi.png"
+              className="h-[70px] w-[70px] mt-[-15px]"
+              alt="iconsaxboldlock"
+            />            
+
+            <Text
+              className="text-left text-white_A700 w-auto"
+              as="h4"
+              variant="h4"
+            >
+              4
+            </Text>
+        </div>
+
+          <Button
+            className="mt-15 cursor-pointer font-bold min-w-[293px] text-center text-white_A700 text-xl w-auto"
+            shape="RoundedBorder8"
+            size="lg"
+            variant="FillRed900"
+          >
+            Go back Home
+          </Button>
+        </div> 
+
+       </div>
 
       </div>
     </>
