6772887235d9092d68adeace7a897f342080e714
atienoobwanda <atienoobwanda@gmail.com>
2023-05-15 04:19:57 +0300

    signup successful page made responsive

--- src/Pages/Users/Auth/SignUpSuccessful.jsx
+++ src/Pages/Users/Auth/SignUpSuccessful.jsx
@@ -10,9 +10,13 @@ import SignupColumnlogotwo from "Components/SignupColumn";
 const SignUpSuccessful = () => {
   return (
     <>
-    <div className="bg-black_900 flex flex-col font-roboto sm:gap-10 md:gap-10 gap-24 items-center justify-start mx-auto pb-[499px] w-full">
+    <div className="bg-black_900 flex flex-col font-roboto sm:gap-10 md:gap-10 gap-24 items-center justify-start mx-auto pb-[850px] w-full md:pr-4 md:pl-4 md:pt-10 sm:pr-8 sm:pl-8">
       <SignupColumnlogotwo className="flex flex-col items-center justify-start p-8 sm:px-5 w-full" />
-      <div className="bg-black_900_01 border border-gray_300 border-solid flex flex-col items-center justify-center px-40 md:px-5 py-12 rounded-lg self-stretch w-auto md:w-full">
+      
+      {/* <div className="bg-black_900_01 border border-gray_300 border-solid flex flex-col items-center justify-center px-40 md:px-5 py-12 rounded-lg self-stretch w-auto md:w-full"> */}
+      <div className="bg-black_900_01 border border-gray_300 border-solid flex flex-col items-center justify-center px-40 md:px-5 py-12 rounded-lg self-stretch w-3/4 md:w-90% md:mt-[130px] sm:w-full  mx-auto">
+      {/* <div className="bg-black_900_01 border border-gray_300 border-solid flex flex-col items-center justify-center px-40 md:px-5 py-12 rounded-lg self-stretch w-3/4 md:w-full mx-auto md:mr-4 md:ml-4"> */}
+
         <div className="flex flex-col items-center justify-start self-stretch w-auto sm:w-full">
           <div className="flex flex-col gap-4 items-center justify-center self-stretch w-auto sm:w-full">
             <Text
@@ -22,7 +26,7 @@ const SignUpSuccessful = () => {
             >
               Sign up successful!
             </Text>
-            <div className="flex flex-col items-start justify-start self-stretch w-auto sm:w-full">
+            <div className="flex flex-col items-center justify-start self-stretch w-auto sm:w-full">
               <Text
                 className="font-normal leading-[175.00%] max-w-[458px] md:max-w-full not-italic text-center text-gray_300_a2"
                 as="h6"
@@ -45,6 +49,8 @@ const SignUpSuccessful = () => {
           </div>
         </div>
       </div>
+    
+    
     </div>
   </>
   );
