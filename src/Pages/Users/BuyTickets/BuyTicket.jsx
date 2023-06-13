77ce7d62ae12dfc9f9428ef810e3a304e9f08d61
atienoobwanda <atienoobwanda@gmail.com>
2023-06-14 02:09:32 +0300

    the calendar scroller works for June

--- src/Pages/Users/BuyTickets/BuyTicket.jsx
+++ src/Pages/Users/BuyTickets/BuyTicket.jsx
@@ -8,70 +8,63 @@ import Navbar from "Layout/Navbar/Navbar";
 import SelectingShowTimeColumn from "UI_Components/SelectingShowTimeColumn";
 import SelectingShowTimeColumn1 from "UI_Components/SelectingShowTimeColumn1";
 import SelectingShowTimeRowa from "UI_Components/SelectingShowTimeRow";
-import BookingCalendar from "Components/BookingCalendar";
+import BookingCalendar from 'Components/Users/BookingCalendar';
 
 
-const getDayOfMonthString = (dayOfMonth) => {
-  const suffixes = ['th', 'st', 'nd', 'rd'];
-  const lastTwoDigits = dayOfMonth % 100;
-  const lastDigit = dayOfMonth % 10;
-  let suffix = suffixes[lastDigit] || suffixes[0];
+// const getDayOfMonthString = (dayOfMonth) => {
+//   const suffixes = ['th', 'st', 'nd', 'rd'];
+//   const lastTwoDigits = dayOfMonth % 100;
+//   const lastDigit = dayOfMonth % 10;
+//   let suffix = suffixes[lastDigit] || suffixes[0];
 
-  // Special case for numbers ending in 11, 12, and 13
-  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
-    suffix = 'th';
-  }
+//   // Special case for numbers ending in 11, 12, and 13
+//   if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+//     suffix = 'th';
+//   }
 
-  return dayOfMonth + suffix;
-};
+//   return dayOfMonth + suffix;
+// };
 
 
 const BuyingTicket = () => {
 
-  const [currentMonth, setCurrentMonth] = useState(moment());
+  // const [currentMonth, setCurrentMonth] = useState(moment());
 
-  const generateCalendar = () => {
-    const startDate = moment(currentMonth).startOf('month');
-    const endDate = moment(currentMonth).endOf('month');
+  // const generateCalendar = () => {
+  //   const startDate = moment(currentMonth).startOf('month');
+  //   const endDate = moment(currentMonth).endOf('month');
 
-    const calendarDays = [];
+  //   const calendarDays = [];
 
-    let currentDate = moment(startDate);
+  //   let currentDate = moment(startDate);
 
-    while (currentDate.isSameOrBefore(endDate)) {
-      const dayOfWeek = currentDate.format('ddd');
-      const dayOfMonth = getDayOfMonthString(currentDate.format('D'));
+  //   while (currentDate.isSameOrBefore(endDate)) {
+  //     const dayOfWeek = currentDate.format('ddd');
+  //     const dayOfMonth = getDayOfMonthString(currentDate.format('D'));
 
-      const imageSrc = 'images/img_group39896.svg';
+  //     const imageSrc = 'images/img_group39896.svg';
 
-      const calendarDay = (
-        // <SelectingShowTimeColumn
-        //   key={currentDate.toString()}
-        //   className="flex flex-col items-center justify-start p-[31px] sm:px-5 w-full"
-        //   language={dayOfWeek + ' '}
-        //   p1st={dayOfMonth}
-        // >
-        //   <img src={imageSrc} alt="" style={{ height: '118px', width: '54px' }} />
-        // </SelectingShowTimeColumn>
-        <SelectingShowTimeColumn
-        key={currentDate.toString()}
-        className="bg-cover bg-no-repeat flex flex-col h-[118px] w-[54px] items-center justify-start p-[4em] sm:px-5"
-        style={{
-          backgroundImage: "url('images/img_group39896.svg')",
-        }}
-        language={dayOfWeek + ' '}
-        p1st={dayOfMonth}
-      />
+  //     const calendarDay = (
+      
+  //       <SelectingShowTimeColumn
+  //       key={currentDate.toString()}
+  //       className="bg-cover bg-no-repeat flex flex-col h-[118px] w-[54px] items-center justify-start p-[4em] sm:px-5"
+  //       style={{
+  //         backgroundImage: "url('images/img_group39896.svg')",
+  //       }}
+  //       language={dayOfWeek + ' '}
+  //       p1st={dayOfMonth}
+  //     />
 
-      );
+  //     );
 
-      calendarDays.push(calendarDay);
+  //     calendarDays.push(calendarDay);
 
-      currentDate.add(1, 'day');
-    }
+  //     currentDate.add(1, 'day');
+  //   }
 
-    return calendarDays;
-  };
+  //   return calendarDays;
+  // };
 
 
   return (
@@ -85,122 +78,10 @@ const BuyingTicket = () => {
         />
 
         <div className="flex flex-col gap-12 items-start justify-start max-w-[1186px] mt-[155px] mx-auto md:px-5 self-stretch w-full">
-          <div className="flex flex-col gap-6 items-start justify-start self-stretch w-auto md:w-full">
-            <Text
-              className="text-left text-white_A700 w-auto"
-              as="h2"
-              variant="h2"
-            >
-              Select show time
-            </Text>
-            <div className="flex flex-col items-start justify-start self-stretch w-auto md:w-full">
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              <div className="bg-black_900_01 flex flex-col gap-4 items-start justify-start max-w-[1186px] p-12 md:px-10 sm:px-5 rounded-lg w-full">
-                <Text
-                  className="font-normal not-italic text-left text-white_A700 w-auto"
-                  variant="body4"
-                >
-                  Select date
-                </Text>
-                <div className="flex flex-col gap-8 items-start justify-start self-stretch w-auto md:w-full">
-                  <div className="flex md:flex-col flex-row gap-2 items-start justify-start self-stretch w-auto md:w-full">
-                    {/* Scroll Left */}
+        {/* Calendar: */}
+         <BookingCalendar/>
 
-                    <Img
-                      src="images/img_group40097.svg"
-                      className="h-[118px] w-[54px] pb-[2em]"
-                      alt="Scroll Left"
-                    />
-
-                    <List className="sm:flex-col flex-row gap-2 grid grid-cols-7 w-[100%] md:w-full" orientation="horizontal">
-                      {generateCalendar()}
-                    </List>
-
-                    {/* Scroll Right */}
-                    <Img
-                      src="images/img_group40096.svg"
-                      className="h-[118px] w-[54px] pt-[2em]"
-                      alt="Scroll Right"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-4 items-start justify-start self-stretch w-auto sm:w-full">
-                    <Text
-                      className="font-normal not-italic text-left text-white_A700 w-auto"
-                      variant="body4"
-                    >
-                      Select time
-                    </Text>
-                    <div className="flex sm:flex-col flex-row gap-8 items-center justify-center self-stretch w-auto sm:w-full">
-                      <Button
-                        className="cursor-pointer font-normal not-italic text-base text-center text-white_A700 w-[108px]"
-                        shape="RoundedBorder4"
-                        size="md"
-                        variant="FillRed900"
-                      >
-                        3:00 PM
-                      </Button>
-                      <Button
-                        className="cursor-pointer font-normal not-italic text-base text-black_900_01 text-center w-[108px]"
-                        shape="RoundedBorder4"
-                        size="md"
-                        variant="FillWhiteA700"
-                      >
-                        6:30 PM
-                      </Button>
-                      <Button
-                        className="cursor-pointer font-normal not-italic text-base text-black_900_01 text-center w-[108px]"
-                        shape="RoundedBorder4"
-                        size="md"
-                        variant="FillWhiteA700"
-                      >
-                        8:30 PM
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            
-            
-            
-            
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-            </div>
-          </div>
+        {/* Seat selection */}
           <div className="flex flex-col gap-6 items-start justify-center self-stretch w-auto md:w-full">
             <Text
               className="text-left text-white_A700 w-auto"
@@ -235,6 +116,7 @@ const BuyingTicket = () => {
               iOne="I"
             />
           </div>
+          {/* Checkout: */}
           <div className="flex flex-col gap-6 items-start justify-center self-stretch w-auto md:w-full">
             <Text
               className="text-left text-white_A700 w-auto"
@@ -337,6 +219,7 @@ const BuyingTicket = () => {
               </div>
             </div>
           </div>
+
         </div>
 
         <Footer
@@ -344,8 +227,6 @@ const BuyingTicket = () => {
           copyrightheartsOne="Copyright Heartstrings Entertainment"
         />
       </div>
-
-
     </>
   );
 };
