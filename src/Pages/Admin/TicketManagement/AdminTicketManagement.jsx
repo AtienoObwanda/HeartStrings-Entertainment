0707bf81f3cd7e5561f5c333f8057aa64d255823
AtienoObwanda <atienoobwanda@gmail.com>
2023-07-03 12:21:49 +0300

    adding the template layout for user management and ticket management

---
+++ src/Pages/Admin/TicketManagement/AdminTicketManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminTicketManagement = () => {
+  return (
+    <div>AdminTicketManagement</div>
+  )
+}
+
+export default AdminTicketManagement
---
+++ src/Pages/Admin/UserManagement/AdminUserManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminUserManagement = () => {
+  return (
+    <div>AdminUserManagement</div>
+  )
+}
+
+export default AdminUserManagement
