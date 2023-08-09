00dcd2ac5d1be867f4262de212c7569976130048
AtienoObwanda <atienoobwanda@gmail.com>
2023-08-09 11:52:00 +0300

    update env

--- src/Pages/Users/Auth/SignUpEmailSent.jsx
+++ src/Pages/Users/Auth/SignUpEmailSent.jsx
@@ -12,7 +12,7 @@ const SignUpEmailSent = () => {
   return (
     <div className="bg-black_900 flex flex-col font-roboto sm:gap-10 md:gap-10 gap-6 items-center justify-start mx-auto pb-[850px] w-full md:pr-4 md:pl-4 md:pt-10 sm:pr-8 sm:pl-8">
     <SignupColumnlogotwo className="flex flex-col mt-8 items-center justify-start p-8 sm:px-5 w-full" />
-    <div className="h-40"></div>
+    <div className="h-40 md:h-0"></div>
     <div className="bg-black_900_01 border border-gray_300 border-solid flex flex-col items-center justify-center px-40 md:px-5 py-12 rounded-lg self-stretch w-1/2 md:w-90% md:mt-[130px] sm:w-full  mx-auto">
 
       <div className="flex flex-col items-center justify-start self-stretch w-auto sm:w-full">
