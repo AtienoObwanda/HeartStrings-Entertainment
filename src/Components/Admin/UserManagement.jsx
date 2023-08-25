3a7054d281cacf69b23e5773e577488a7e3a0815
AtienoObwanda <atienoobwanda@gmail.com>
2023-08-25 16:18:35 +0300

    delete user module is ready

--- src/Components/Admin/UserManagement.jsx
+++ src/Components/Admin/UserManagement.jsx
@@ -16,6 +16,8 @@ const UserManagement = () => {
   const [users, setUsers] = useState([]);
   const accessToken = localStorage.getItem('accessToken');
   const [openModalId, setOpenModalId] = useState(null);
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+
 
   useEffect(() => {
     const headers = {
@@ -107,22 +109,22 @@ const UserManagement = () => {
     setIsDeleteConfirmationOpen(false);
   };
 
-  // const handleDeleteUser = () => {
-
-    // closeViewModal();
-    // closeDeleteConfirmation();
-  // };
+  
 
   const handleDeleteUser = async (userId) => {
     try {
       const headers = {
-        Authorization: `Bearer ${accessToken}`, // Replace with your authentication token
+        Authorization: `Bearer ${accessToken}`, 
       };
   
       const response = await axios.delete(`${apiUrl}/api/delete-user/${userId}/`, { headers });
 
       if (response.status === 204) {
         console.log('User deleted successfully.');
+        setShowSuccessAlert(true);
+        setTimeout(() => {
+          setShowSuccessAlert(false); // Hide the success alert after a brief delay
+        }, 3000); // Set  success alert visibility
       } else {
         console.log('Failed to delete user.');
        
@@ -146,6 +148,19 @@ const UserManagement = () => {
     <>
     
     <div class="sm:hidden relative overflow-hidden w-full pb-[35em]">
+       {/* Success Alert */}
+    {showSuccessAlert && (
+        <div class="relative flex flex-col sm:flex-row sm:items-center bg-white w-3/4 shadow rounded-md py-5 pl-6 pr-8 sm:pr-6">
+                  <div class="flex flex-row items-center border-b sm:border-b-0 w-full sm:w-auto pb-4 sm:pb-0">
+                    <div class="text-green-500">
+                      <svg class="w-6 sm:w-5 h-6 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    </div>
+                    <div class="text-sm font-medium ml-3">Success!.</div>
+                  </div>
+                  <div class="text-sm tracking-wide text-gray-500 mt-4 sm:mt-0 sm:ml-4">User Has been deleted Suceessfully!!!</div>
+                 
+                </div>
+      )}
         <table class="w-full text-sm text-left text-white_400">
             <thead class="text-xs uppercase bg-gray_800 text-white_A700">
                 <tr>
@@ -325,6 +340,8 @@ const UserManagement = () => {
             </tfoot>
         </table>
     </div>
+
+   
     
         </>
           );
--- src/Components/Admin/confirmationModal.jsx
+++ src/Components/Admin/confirmationModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Img, Input, Text } from "UI_Components";
-
+import {PiWarningCircleThin} from 'react-icons/pi'
 const ConfirmationModal = ({ isOpen, onCancel, onConfirm }) => {
   if (!isOpen) {
     return null;
@@ -8,7 +8,7 @@ const ConfirmationModal = ({ isOpen, onCancel, onConfirm }) => {
 
   return (
   
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center pt-[6em] justify-center z-50 bg-black bg-opacity-50">
     <div className="bg-black_900_01 flex flex-col items-start justify-start p-[10px] pl-[10px] pr-[100px] pb-[60px] md:px-5 rounded-lg w-auto md:w-full">
 
         {/* <Img className="h-12 w-12" src="#" alt="Close" 
@@ -16,16 +16,27 @@ const ConfirmationModal = ({ isOpen, onCancel, onConfirm }) => {
             /> */}
             
             <div className="flex flex-col gap-12 items-center pl-[110px] text-center justify-center w-auto">
-                <div className="flex flex-col items-center justify-center text-center w-auto">
-                <Text className="font-bold text-white-A700 text-xl w-auto">Delete User</Text>
+                <div className="flex items-center justify-center text-center w-auto">
+                <Text className="font-bold text-white-A700 text-xl w-auto">
+                  Delete User</Text>
                  </div>
+
          </div>
         <div className="bg-black_900_01 rounded-lg p-6 gap-4 pl-[60px]">
             <Text className="text-base text-white_A700 w-auto">Are you sure you want to delete this user?</Text>
             <br></br>
+            <div className="flex items-center mt-4 pl-3">
+              <PiWarningCircleThin className="h-6 w-6 text-red_900_01" />
+              <Text className="text-sm text-white_A700 ml-2">User will be deleted permanently!</Text>
+            </div>
+
+             
+            <br></br>
+
+
             <button
             onClick={onCancel}
-            className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded mr-[40px] mt-[3em]"
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded mr-[60px] mt-[2.5em]"
 
             >
             Cancel
