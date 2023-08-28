416a10b7dbd39a33af11a0f67d29ac9c02c82c13
AtienoObwanda <atienoobwanda@gmail.com>
2023-08-28 20:10:28 +0300

    env files

---
+++ env.js
@@ -0,0 +1,5 @@
+export const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+// ID
+export const googleOAuth2Key = import.meta.env.VITE_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY;
+// SECRET
+export const googleOAuth2Secret = import.meta.env.VITE_SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET;
