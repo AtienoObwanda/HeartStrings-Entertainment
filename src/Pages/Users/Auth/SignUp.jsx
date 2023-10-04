050b1916ecd481d5ee4e72c3fe03fd78b730a829
AtienoObwanda <atienoobwanda@gmail.com>
2023-10-04 08:40:56 +0300

    Form Validation for email and pasword sorted

--- src/Pages/Users/Auth/SignUp.jsx
+++ src/Pages/Users/Auth/SignUp.jsx
@@ -30,6 +30,8 @@ console.log(`${apiUrl}/auth/o/google-oauth2/?redirect_uri=${redirectUri}&client_
 const SignUp = ({  }) => {
   const navigate = useNavigate();
   // const history = useHistory();
+  const [alertMessage, setAlertMessage] = useState(null);
+
 
   const [first_name, setFirst_name] = useState('');
   const [last_name, setLast_name] = useState('');
@@ -38,8 +40,10 @@ const SignUp = ({  }) => {
   const [password, setPassword] = useState('');
   const [re_password, setRe_password] = useState('');
 
+  
   const onSubmit = async (e) => {
     e.preventDefault();
+    
   
     if (password === re_password) {
       try {
@@ -54,18 +58,23 @@ const SignUp = ({  }) => {
         });
 
           if (response.status >= 200 && response.status < 300) {
-
-          console.log('Data posted successfully.');
-          // setAccountCreated(true);
-          navigate('/activate-account');// Render Navigate component
-        } else {
-          console.log(response.data.message);
-        }
+          navigate('/activate-account');
+        }  else {
+          
+            console.log(response.data.message);        
+          }
       } catch (error) {
-        console.error('An error occurred:', error);
-        console.error('Response:', error.response);
-        console.log('Error message:', error.response && error.response.data && error.response.data.error.message);
+        
+        // console.error('An error occurred:', error);
+        // console.error('Response:', error.response);
+        // console.log('Error message:', error.response && error.response.data && error.response.data.error.message);
 
+        if (error.response && error.response.data && error.response.data.email) {
+          // window.alert(error.response.data.email[0]);
+          setAlertMessage(error.response.data.email[0]);
+
+
+        }
       }
     } else {
       console.log("Passwords don't match");
@@ -101,7 +110,15 @@ const SignUp = ({  }) => {
           <div className="flex flex-col gap-4 items-start justify-start self-stretch w-auto sm:w-full">
             <div className="flex flex-col gap-12 items-center justify-center self-stretch w-auto sm:w-full">
             
-            
+            {alertMessage && (
+        <div className="flex items-center bg-red_900 text-white text-sm font-bold px-4 py-3" role="alert">
+          <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/>
+          </svg>
+          <p>{alertMessage}</p>
+
+        </div>
+      )}
             
             {/* {accountCreated ? (
             <p className='bg-light_blue_500 text-white_A700 W-40 H-40'>Account Created Successfully. An activation email has been sent.</p>
@@ -192,6 +209,7 @@ const SignUp = ({  }) => {
                         type="password"
                         placeholder="Password"
                         onChange={(e) => setPassword(e.target.value)}
+                        x-model="password"
                         required
                      />
                       </div>
@@ -208,9 +226,43 @@ const SignUp = ({  }) => {
                         type="password"
                         placeholder="Confirm Password"
                         onChange={(e) => setRe_password(e.target.value)}
+                        x-model="re_password"
                         required
                      />
                     </div>
+                    <div className="flex justify-start mt-3 ml-4 p-1">
+                      <ul>
+                        <li className="flex items-center py-1">
+                          <div className={`rounded-full p-1 fill-current ${password === re_password && password.length > 0 ? 'bg-green-200 text-green-700' : 'bg-red-200 text-red-700'}`}>
+                            <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              {password === re_password && password.length > 0 ? (
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                              ) : (
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                              )}
+                            </svg>
+                          </div>
+                          <span className={`font-medium text-sm ml-3 ${password === re_password && password.length > 0 ? 'text-green-700' : 'text-red-700'}`}>
+                            {password === re_password && password.length > 0 ? 'Passwords match' : 'Passwords do not match'}
+                          </span>
+                        </li>
+                        <li className="flex items-center py-1">
+                          <div className={`rounded-full p-1 fill-current ${password.length > 7 ? 'bg-green-200 text-green-700' : 'bg-red-200 text-red-700'}`}>
+                            <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              {password.length > 7 ? (
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                              ) : (
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                              )}
+                            </svg>
+                          </div>
+                          <span className={`font-medium text-sm ml-3 ${password.length > 7 ? 'text-green-700' : 'text-red-700'}`}>
+                            {password.length > 7 ? 'The required length reached' : 'At least 8 characters required'}
+                          </span>
+                        </li>
+                      </ul>
+                    </div>
+                    
 
                     <Button
                       className="cursor-pointer font-bold text-center text-white_A700 text-xl w-[430px] sm:w-full mt-2"
